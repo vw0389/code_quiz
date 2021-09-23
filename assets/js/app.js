6cb897e17fe6a5d6582e00abe8a1006ea9ec5f80
Victor Weinert <victor.weinert@protonmail.com>
2021-09-23 13:41:48 -0500

    added initial countdown & got JSON into array

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,51 @@
-// Timer functionality
-function countdown(seconds) {
-    function tick(wrongQuestion) {
-        var time = document.getElementById("time");
-        if (wrongQuestion === true) {
-            if (seconds <= 5) {
-                seconds = 0;
-            } else {
-                seconds = seconds - 5;
-            }
-        } else {
-            seconds--;
-        }
-        
-        time.innerHTML = (seconds < 10 ? "0" : "") + String(seconds);
-        if( seconds > 0 ) {
-            setTimeout(tick, 1000);
-        }
-    }
-    tick(false);
+var timeEl = document.querySelector('#time');
+var mainEl = document.querySelector('#main');
+var startQuizEl = document.querySelector('#start');
+var highScoreEl = document.querySelector('#highscores');
+var score = 0;
+var questions = getQuestions();
+function getQuestions() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "./assets/js/questions.json", false);
+    request.send(null)
+    var my_JSON_object = JSON.parse(request.responseText);
+    return my_JSON_object;
 }
+function countdown(timeLeft) {
+    var timeInterval = setInterval(function () {
+        var text = "";
+        if (timeLeft > 1) {
+            text = 'Time: ' + timeLeft + ' seconds remaining';
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            text = 'Time: ' + timeLeft + ' second remaining';
+            timeLeft--;
+        } else {
+            clearInterval(timeInterval);
+            timerEndedDoSomething();
+        }
+        timeEl.textContent = text;
+    }, 1000);
+}
+function timerEndedDoSomething() {
+
+}
+
+function toggleHighScores() {
+    // Toggled high scores, stop timer, stop IP quiz and display high scores
+}
+function oneWrong() {
+
+}
+function startQuiz() {
+
+}
+function loadHighscores() {
+
+}
+highScoreEl.addEventListener('click', function () {
+    toggleHighScores();
+})
+startQuizEl.addEventListener('click', function () {
+    startQuiz();
+})
